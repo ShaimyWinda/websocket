03099d6a3b9e5aec4ff5a94f26fd6fe992f4dfb9
Winda <azis.widad@gmail.com>
2020-05-04 14:39:12 +0200

    Etape 1 2 3

--- websocket-server.js
+++ websocket-server.js
@@ -1,1 +1,20 @@
 const WebSocket = require('ws');
+// Etape 1
+const wss = new WebSocket.Server({ port: process.env.WS_PORT });
+
+wss.on('connection', (ws) => {
+    console.log('connected');
+    // Etape 2
+    ws.on('close', function() {
+        console.log('disconnected');
+    });
+    // Etape 3
+    ws.onmessage = (messageEvent) => {
+        const message = messageEvent.data;
+        console.log('message received ', message);
+    };
+});
+
+wss.on('error', (error) => {
+    console.log('error occured ', error);
+});
